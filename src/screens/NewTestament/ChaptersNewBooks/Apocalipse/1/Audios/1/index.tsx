084af2964d08caeacaea44c0375ccd98ad1908bf
victor ciechovicz <victor09ciechovicz@outlook.com>
2022-12-08 22:25:27 -0300

    up

--- src/screens/NewTestament/ChaptersNewBooks/Apocalipse/1/Audios/1/index.tsx
+++ src/screens/NewTestament/ChaptersNewBooks/Apocalipse/1/Audios/1/index.tsx
@@ -1,31 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, ChappterWrapper } from './styles'
 import { useNavigation } from '@react-navigation/native'
 
 import { Header } from '../../../../../../../components/Header'
 import { MusicPlayer } from '../../../../../../../components/MusicPlayer'
 
+import {Audio} from 'expo-av';
+
+
 export function ApocalipseAudio1() {
+  const [sound, setSound] = useState()
+
   const navigation = useNavigation()
 
+ 
   function handleGoback() {
     navigation.goBack()
   }
+
+
+  async function  playSound() {
+   await Audio.Sound.createAsync(
+      {uri:'https://storage.googleapis.com/rota66audio/rota66_001.mp3'},
+      {shouldPlay:true}
+    )
+  }
+
+ 
+
   return (
     <Container>
-      <Header onPress={handleGoback}/>
+      <Header onPress={handleGoback} />
 
-        <ChappterWrapper>
-          <MusicPlayer
-            title="Estudo 200 - Crônicas 1-6"
-            nameButtonCenter="playcircleo"
-            onPressButtonCenter={() => {}}
-            onPressButtonBack={() => {}}
-            onPressButtonPass={() => {}}
-           
-          />
-        </ChappterWrapper>
- 
+      <ChappterWrapper>
+        <MusicPlayer
+          title="Estudo 200 - Crônicas 1-6"
+          nameButtonCenter="playcircleo"
+          onPressButtonCenter={playSound}
+          onPressButtonBack={() => {}}
+          onPressButtonPass={() => {}}
+        />
+      </ChappterWrapper>
     </Container>
   )
 }
