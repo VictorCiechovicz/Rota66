9f2d2bb8482c15ab81e22f02b926f008d10acc39
victor ciechovicz <victor09ciechovicz@outlook.com>
2022-12-10 19:47:02 -0300

    up

--- src/screens/NewTestament/ChaptersNewBooks/Apocalipse/1/Audios/1/index.tsx
+++ src/screens/NewTestament/ChaptersNewBooks/Apocalipse/1/Audios/1/index.tsx
@@ -5,7 +5,7 @@ import { useNavigation } from '@react-navigation/native'
 import { Header } from '../../../../../../../components/Header'
 import { MusicPlayer } from '../../../../../../../components/MusicPlayer'
 
-import {Audio} from 'expo-av';
+
 
 
 export function ApocalipseAudio1() {
@@ -19,13 +19,35 @@ export function ApocalipseAudio1() {
   }
 
 
-  async function  playSound() {
-   await Audio.Sound.createAsync(
-      {uri:'https://storage.googleapis.com/rota66audio/rota66_001.mp3'},
-      {shouldPlay:true}
-    )
-  }
-
+  const songs = [
+    {
+      id: 1,
+      title: 'Estudo 200 - Crônicas 1-5',
+      url: 'https://storage.googleapis.com/rota66audio/rota66_001.mp3'
+    },
+    {
+      id: 2,
+      title: 'Estudo 200 - Crônicas 2-5',
+      url: 'https://storage.googleapis.com/rota66audio/rota66_002.mp3'
+    },
+    {
+      id: 3,
+      title: 'Estudo 200 - Crônicas 3-5',
+      url: 'https://storage.googleapis.com/rota66audio/rota66_003.mp3'
+    },
+    {
+      id: 4,
+      title: 'Estudo 200 - Crônicas 4-5',
+      url: 'https://storage.googleapis.com/rota66audio/rota66_004.mp3'
+    },
+    {
+      id: 5,
+      title: 'Estudo 200 - Crônicas 5-5',
+      url: 'https://storage.googleapis.com/rota66audio/rota66_005.mp3'
+    }
+  ]
+  
+ 
  
 
   return (
@@ -36,7 +58,7 @@ export function ApocalipseAudio1() {
         <MusicPlayer
           title="Estudo 200 - Crônicas 1-6"
           nameButtonCenter="playcircleo"
-          onPressButtonCenter={playSound}
+          onPressButtonCenter={() => {}}
           onPressButtonBack={() => {}}
           onPressButtonPass={() => {}}
         />
