d85dc69a08ad3dc9523802262d025fd9db6eafb1
victor ciechovicz <victor09ciechovicz@outlook.com>
2022-12-10 20:43:05 -0300

    up

--- src/screens/NewTestament/ChaptersNewBooks/Apocalipse/1/Audios/1/index.tsx
+++ src/screens/NewTestament/ChaptersNewBooks/Apocalipse/1/Audios/1/index.tsx
@@ -5,18 +5,50 @@ import { useNavigation } from '@react-navigation/native'
 import { Header } from '../../../../../../../components/Header'
 import { MusicPlayer } from '../../../../../../../components/MusicPlayer'
 
-
-
+import SoundPlayer from 'react-native-sound-player'
 
 export function ApocalipseAudio1() {
   const [sound, setSound] = useState()
 
   const navigation = useNavigation()
 
- 
   function handleGoback() {
     navigation.goBack()
   }
+
+  function playSong() {
+    try {
+      SoundPlayer.playUrl(
+        'https://storage.googleapis.com/rota66audio/rota66_001.mp3'
+      )
+    } catch (e) {
+      console.log(`cannot play the sound file`, e)
+    }
+  }
+
+  async function getInfo() {
+    try {
+      const info = await SoundPlayer.getInfo() // Also, you need to await this because it is async
+      console.log('getInfo', info) // {duration: 12.416, currentTime: 7.691}
+    } catch (e) {
+      console.log('There is no song playing', e)
+    }
+  }
+
+  function onPressPlayButton() {
+    playSong()
+    getInfo()
+  }
+
+function stop(){
+  SoundPlayer.pause()
+}
+
+function con(){
+  SoundPlayer.resume()
+}
+
+
 
 
   const songs = [
@@ -46,9 +78,6 @@ export function ApocalipseAudio1() {
       url: 'https://storage.googleapis.com/rota66audio/rota66_005.mp3'
     }
   ]
-  
- 
- 
 
   return (
     <Container>
@@ -58,9 +87,9 @@ export function ApocalipseAudio1() {
         <MusicPlayer
           title="Estudo 200 - Crônicas 1-6"
           nameButtonCenter="playcircleo"
-          onPressButtonCenter={() => {}}
-          onPressButtonBack={() => {}}
-          onPressButtonPass={() => {}}
+          onPressButtonCenter={onPressPlayButton}
+          onPressButtonBack={con}
+          onPressButtonPass={stop}
         />
       </ChappterWrapper>
     </Container>
